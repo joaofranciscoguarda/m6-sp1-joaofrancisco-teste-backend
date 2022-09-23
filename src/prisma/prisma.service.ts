39f967c71558d62e7cd5c60c31c416cd955343c0
João Francisco <joaofrancisco.guarda@gmail.com>
2022-09-23 11:34:01 -0300

    feat: create cleanDb function, to clean up the db as soon as the tests start

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: "postgresql://postgres:1234@localhost:5432/nest?schema=public"
+          url: config.get('DATABASE_URL'),
         },
       },
     });
   }
 
-  
+  cleanDb() {
+    return this.$transaction([
+      this.contact.deleteMany(),
+      this.user.deleteMany(),
+    ]);
+  }
 }
