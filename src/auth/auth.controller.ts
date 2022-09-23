599cbdc215c730e41fc640c860eca45d02cdc58b
João Francisco <joaofrancisco.guarda@gmail.com>
2022-09-23 11:30:42 -0300

    feat: created routes for signup and signin, as token return for signin

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,30 @@
-import { Body, Controller, Post, Req } from "@nestjs/common";
-import { Request } from "express";
-import { AuthService } from "./auth.service";
-import { AuthDto } from "./dto";
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import {
+  CreateUserDto,
+  LoginUserDto,
+} from './dto';
 
 @Controller('auth')
-export class AuthController{
-  constructor(private authService:AuthService) {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
 
-    @Post('signup')
-    signup(@Body() dto:AuthDto){
-      console.log({dto})
-      return this.authService.signup(dto)
-    }
+  // @HttpCode(HttpStatus.CREATED) Default pra post é 201
+  @Post('signup')
+  signup(@Body() dto: CreateUserDto) {
+    console.log({ dto });
+    return this.authService.signup(dto);
+  }
 
-    @Post('signin')
-    signin(){
-      return this.authService.signin
-    }
-  
+  @HttpCode(HttpStatus.OK)
+  @Post('signin')
+  signin(@Body() dto: LoginUserDto) {
+    return this.authService.signin(dto);
+  }
 }
