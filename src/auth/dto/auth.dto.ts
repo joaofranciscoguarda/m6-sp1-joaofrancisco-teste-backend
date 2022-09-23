b79b8be6712dc00b0957dc078e51e29b398b0d82
João Francisco <joaofrancisco.guarda@gmail.com>
2022-09-23 10:58:27 -0300

    feat: changed AuthDto to CreateUserDto and add a LoginUserDto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,16 +1,29 @@
-import {IsEmail, IsNotEmpty, IsString} from 'class-validator'
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
 
-export class AuthDto {
+export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
-  fullName: string
+  fullName: string;
 
   @IsEmail()
   email: string;
 
   @IsString()
   @IsNotEmpty()
-  cellphone: string
+  cellphone: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
+
+export class LoginUserDto {
+  @IsEmail()
+  email: string;
 
   @IsString()
   @IsNotEmpty()
